9aa1c8399ddb567e5fbbebb3c2d1e8b5f4e49dc1
David Himmelstrup <lemmih@gmail.com>
2026-03-08 09:41:05 +0800

    Make TypeApplications no-space case pass

--- components/haskell-parser/test/Test/Fixtures/TypeApplications/type-applications-expr-basic.hs
+++ components/haskell-parser/test/Test/Fixtures/TypeApplications/type-applications-expr-basic.hs
@@ -1,7 +1,7 @@
 module TypeApplicationsExprBasic where
 
-f :: a -> a
-f x = x
+f :: a -> b -> a
+f x _ = x
 
-h :: (Int, Int, Int, Int, Int)
-h = (f @Int 1, f 2 @Int, f @Int @Int 3, f @(Maybe Int) (Just 4), ((+) @Int) 5 6)
+x :: Int
+x = f 2 @Int 3
