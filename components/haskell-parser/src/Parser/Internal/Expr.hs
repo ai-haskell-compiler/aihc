73e3b9556dcf6cea25069efbdbf244324256d2a4
David Himmelstrup <lemmih@gmail.com>
2026-03-11 09:27:46 +0800

    feat(parser): support qualified, aliased, and hiding imports

--- components/haskell-parser/src/Parser/Internal/Expr.hs
+++ components/haskell-parser/src/Parser/Internal/Expr.hs
@@ -131,7 +131,8 @@ appExprParser = withSpan $ do
 
 atomExprParser :: TokParser Expr
 atomExprParser =
-  parenExprParser
+  MP.try parenOperatorExprParser
+    <|> parenExprParser
     <|> listExprParser
     <|> intBaseExprParser
     <|> floatExprParser
@@ -139,6 +140,16 @@ atomExprParser =
     <|> charExprParser
     <|> stringExprParser
     <|> varExprParser
+
+parenOperatorExprParser :: TokParser Expr
+parenOperatorExprParser = withSpan $ do
+  symbolLikeTok "("
+  op <- tokenSatisfy $ \tok ->
+    case lexTokenKind tok of
+      TkOperator sym -> Just sym
+      _ -> Nothing
+  symbolLikeTok ")"
+  pure (`EVar` op)
 
 patternParser :: TokParser Pattern
 patternParser = withSpan $ do
