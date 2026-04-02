c5475f26334fb3855fcf8771d176ae40c9c66c9c
David Himmelstrup <lemmih@gmail.com>
2026-04-02 06:28:50 +0200

    fix(test): restore original promoted kind fixture

--- components/aihc-parser/test/Test/Fixtures/oracle/DataKinds/list-promoted-kind.hs
+++ components/aihc-parser/test/Test/Fixtures/oracle/DataKinds/list-promoted-kind.hs
@@ -1,6 +1,7 @@
 {- ORACLE_TEST pass -}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeOperators #-}
 module ListPromotedKind where
 
-type T = a ': b
+import Data.Proxy
+fn :: Proxy (() ': '[])
+fn = undefined
