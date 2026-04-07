a8d3aa19d690b4b2d55135cf3b90543d3d72e66d
David Himmelstrup <lemmih@gmail.com>
2026-04-07 16:31:28 +0200

    fix(parser): support overloaded label expressions

--- components/aihc-parser/src/Aihc/Parser/Syntax.hs
+++ components/aihc-parser/src/Aihc/Parser/Syntax.hs
@@ -1386,6 +1386,7 @@ data Expr
   | ECharHash SourceSpan Char Text
   | EString SourceSpan Text Text
   | EStringHash SourceSpan Text Text
+  | EOverloadedLabel SourceSpan Text Text
   | EQuasiQuote SourceSpan Text Text
   | EIf SourceSpan Expr Expr Expr
   | EMultiWayIf SourceSpan [GuardedRhs]
@@ -1441,6 +1442,7 @@ instance HasSourceSpan Expr where
       ECharHash span' _ _ -> span'
       EString span' _ _ -> span'
       EStringHash span' _ _ -> span'
+      EOverloadedLabel span' _ _ -> span'
       EQuasiQuote span' _ _ -> span'
       EIf span' _ _ _ -> span'
       EMultiWayIf span' _ -> span'
