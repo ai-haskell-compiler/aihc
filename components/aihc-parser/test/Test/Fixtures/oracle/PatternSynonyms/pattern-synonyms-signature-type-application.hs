844aa7d94e8f24431f6e72640d586bd2f8df242d
David Himmelstrup <lemmih@gmail.com>
2026-04-26 13:28:21 +0200

    test: add missing extensions to pattern-synonyms-signature-type-application
    The oracle (GHC) rejected the file because `forall` in a type requires
    ExplicitForAll, `{k :: Type}` requires KindSignatures + an import of
    Data.Kind.Type, and kind-polymorphic `k` requires PolyKinds. Also add
    imports for Data.Typeable and Type.Reflection so the referenced names
    are in scope.

--- components/aihc-parser/test/Test/Fixtures/oracle/PatternSynonyms/pattern-synonyms-signature-type-application.hs
+++ components/aihc-parser/test/Test/Fixtures/oracle/PatternSynonyms/pattern-synonyms-signature-type-application.hs
@@ -1,5 +1,9 @@
 {- ORACLE_TEST pass -}
-{-# LANGUAGE PatternSynonyms, TypeApplications #-}
+{-# LANGUAGE ExplicitForAll, KindSignatures, PatternSynonyms, PolyKinds, TypeApplications #-}
 module PatternSynonymsSignatureTypeApplication where
+
+import Data.Kind (Type)
+import Data.Typeable (Typeable)
+import Type.Reflection (TypeRep)
 
 pattern TypeRep :: forall {k :: Type} (a :: k). () => Typeable @k a => TypeRep @k a
