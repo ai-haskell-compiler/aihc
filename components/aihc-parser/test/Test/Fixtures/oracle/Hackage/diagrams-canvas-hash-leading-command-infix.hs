427db18c0494809840a53a3f36336b930e260884
David Himmelstrup <lemmih@gmail.com>
2026-05-03 16:17:02 +0200

    test(parser): add hash-leading command infix oracle fixture

---
+++ components/aihc-parser/test/Test/Fixtures/oracle/Hackage/diagrams-canvas-hash-leading-command-infix.hs
@@ -0,0 +1,11 @@
+{-# ORACLE_TEST pass -}
+{-# LANGUAGE Arrows #-}
+module DiagramsCanvasCommandHashInfix where
+
+import Control.Arrow
+
+expr' term = proc x -> do
+  returnA -< x
+  #> do
+    y <- term -< ()
+    expr' term -< x + y
