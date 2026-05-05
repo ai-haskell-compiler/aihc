457eb80d5deb5500cfca19c3bb5b96546a80349b
David Himmelstrup <lemmih@gmail.com>
2026-05-05 11:07:58 +0200

    feat(parser): emit comment tokens (#1103)

--- bin/aihc-fmt/src/Aihc/Fmt/Comment.hs
+++ bin/aihc-fmt/src/Aihc/Fmt/Comment.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
 
 module Aihc.Fmt.Comment
   ( CommentKind (..),
@@ -10,11 +9,14 @@ module Aihc.Fmt.Comment
   )
 where
 
+import Aihc.Parser.Lex
+  ( LexToken (..),
+    LexTokenKind (..),
+    TokenOrigin (..),
+    lexModuleTokensWithSourceNameAndExtensions,
+  )
 import Aihc.Parser.Syntax (SourceSpan (..))
-import Data.Char (GeneralCategory (..), generalCategory, isAlphaNum, isSpace)
-import Data.Text (Text, pattern Empty, pattern (:<))
-import Data.Text qualified as T
-import Data.Text.Encoding qualified as TE
+import Data.Text (Text)
 
 data CommentKind
   = LineComment
@@ -43,189 +45,56 @@ formatCommentScanError err =
     UnterminatedCharLiteral sp -> "unterminated character literal at " <> formatSpan sp
 
 collectComments :: FilePath -> Text -> Either CommentScanError [SourceComment]
-collectComments sourceName input = go initialState []
+collectComments sourceName input = go Nothing [] tokens
   where
-    initialState =
-      ScanState
-        { scanInput = input,
-          scanLine = 1,
-          scanCol = 1,
-          scanOffset = 0,
-          scanLineHasCode = False
-        }
+    tokens = lexModuleTokensWithSourceNameAndExtensions sourceName [] input
 
-    go st acc =
-      case scanInput st of
-        Empty -> Right (reverse acc)
-        chars
-          | Just st' <- consumeWhitespace st ->
-              go st' acc
-          | "{-#" `T.isPrefixOf` chars,
-            Just st' <- consumePragma st ->
-              go st' acc
-          | "{-" `T.isPrefixOf` chars ->
-              case consumeNestedBlockComment sourceName st of
-                Left err -> Left err
-                Right (comment, st') -> go st' (comment : acc)
-          | Just rest <- T.stripPrefix "--" chars,
-            isLineCommentRest rest ->
-              let (comment, st') = consumeLineComment sourceName st
-               in go st' (comment : acc)
-          | "\"" `T.isPrefixOf` chars ->
-              case consumeQuoted sourceName '"' UnterminatedStringLiteral st of
-                Left err -> Left err
-                Right st' -> go (markCode st') acc
-          | "'" `T.isPrefixOf` chars ->
-              case consumeQuoted sourceName '\'' UnterminatedCharLiteral st of
-                Left err -> Left err
-                Right st' -> go (markCode st') acc
+    go _lineHasCode acc [] = Right (reverse acc)
+    go lineHasCode acc (tok : rest) =
+      case lexTokenKind tok of
+        TkLineComment ->
+          let comment = mkComment lineHasCode LineComment tok
+           in go lineHasCode (comment : acc) rest
+        TkBlockComment ->
+          let comment = mkComment lineHasCode BlockComment tok
+           in go lineHasCode (comment : acc) rest
+        TkError "unterminated block comment" ->
+          Left (UnterminatedBlockComment (lexTokenSpan tok))
+        _
+          | tokenCountsAsCode tok ->
+              go (sourceSpanEndLineMaybe (lexTokenSpan tok)) acc rest
           | otherwise ->
-              go (markCode (advanceOne st)) acc
+              go lineHasCode acc rest
 
-data ScanState = ScanState
-  { scanInput :: !Text,
-    scanLine :: !Int,
-    scanCol :: !Int,
-    scanOffset :: !Int,
-    scanLineHasCode :: !Bool
-  }
-  deriving (Eq, Show)
-
-consumeWhitespace :: ScanState -> Maybe ScanState
-consumeWhitespace st =
-  case scanInput st of
-    c :< _
-      | isSpace c ->
-          Just (advanceOne st)
-    _ -> Nothing
-
-consumePragma :: ScanState -> Maybe ScanState
-consumePragma st =
-  let (prefix, suffix) = T.breakOn "#-}" (scanInput st)
-   in if T.null suffix
-        then Nothing
-        else Just (advanceText (prefix <> "#-}") st)
-
-consumeLineComment :: FilePath -> ScanState -> (SourceComment, ScanState)
-consumeLineComment sourceName st =
-  let (raw, rest) = T.break (== '\n') (scanInput st)
-      st' = advanceText raw st
-      span' = mkScanSpan sourceName st st'
-      comment =
-        SourceComment
-          { commentSpan = span',
-            commentText = raw,
-            commentKind = LineComment,
-            commentHasCodeBefore = scanLineHasCode st
-          }
-   in (comment, st' {scanInput = rest})
-
-consumeNestedBlockComment :: FilePath -> ScanState -> Either CommentScanError (SourceComment, ScanState)
-consumeNestedBlockComment sourceName st =
-  case scanBlock 0 0 (scanInput st) of
-    Nothing -> Left (UnterminatedBlockComment (mkPointSpan sourceName st))
-    Just len ->
-      let raw = T.take len (scanInput st)
-          st' = advanceText raw st
-          comment =
-            SourceComment
-              { commentSpan = mkScanSpan sourceName st st',
-                commentText = raw,
-                commentKind = BlockComment,
-                commentHasCodeBefore = scanLineHasCode st
-              }
-       in Right (comment, st')
-  where
-    scanBlock :: Int -> Int -> Text -> Maybe Int
-    scanBlock depth n input =
-      case T.uncons input of
-        Nothing -> Nothing
-        Just (c, rest1) ->
-          case T.uncons rest1 of
-            Just (c2, rest2)
-              | c == '{' && c2 == '-' ->
-                  scanBlock (depth + 1) (n + 2) rest2
-              | c == '-' && c2 == '}' ->
-                  if depth == 1
-                    then Just (n + 2)
-                    else scanBlock (depth - 1) (n + 2) rest2
-            _ -> scanBlock depth (n + 1) rest1
-
-consumeQuoted ::
-  FilePath ->
-  Char ->
-  (SourceSpan -> CommentScanError) ->
-  ScanState ->
-  Either CommentScanError ScanState
-consumeQuoted sourceName quote mkErr st =
-  go False (advanceOne st)
-  where
-    go escaped current =
-      case scanInput current of
-        Empty -> Left (mkErr (mkPointSpan sourceName st))
-        c :< _
-          | escaped -> go False (advanceOne current)
-          | c == '\\',
-            quote == '"',
-            Just afterGap <- consumeStringGap (advanceOne current) ->
-              go False afterGap
-          | c == '\\' -> go True (advanceOne current)
-          | c == quote -> Right (advanceOne current)
-          | c == '\n' -> Left (mkErr (mkPointSpan sourceName st))
-          | otherwise -> go False (advanceOne current)
-
-consumeStringGap :: ScanState -> Maybe ScanState
-consumeStringGap = go False False
-  where
-    go seenWhitespace seenNewline st =
-      case scanInput st of
-        c :< _
-          | c == '\\' && seenWhitespace && seenNewline -> Just (advanceOne st)
-          | isSpace c -> go True (seenNewline || c == '\n') (advanceOne st)
-        _ -> Nothing
-
-advanceOne :: ScanState -> ScanState
-advanceOne st =
-  case T.uncons (scanInput st) of
-    Nothing -> st
-    Just (c, rest) ->
-      let bytes = byteLength (T.singleton c)
-       in if c == '\n'
-            then
-              st
-                { scanInput = rest,
-                  scanLine = scanLine st + 1,
-                  scanCol = 1,
-                  scanOffset = scanOffset st + bytes,
-                  scanLineHasCode = False
-                }
-            else
-              st
-                { scanInput = rest,
-                  scanCol = scanCol st + 1,
-                  scanOffset = scanOffset st + bytes
-                }
-
-advanceText :: Text -> ScanState -> ScanState
-advanceText txt st = T.foldl' (\acc _ -> advanceOne acc) st txt
-
-markCode :: ScanState -> ScanState
-markCode st = st {scanLineHasCode = True}
-
-mkScanSpan :: FilePath -> ScanState -> ScanState -> SourceSpan
-mkScanSpan sourceName start end =
-  SourceSpan
-    { sourceSpanSourceName = sourceName,
-      sourceSpanStartLine = scanLine start,
-      sourceSpanStartCol = scanCol start,
-      sourceSpanEndLine = scanLine end,
-      sourceSpanEndCol = scanCol end,
-      sourceSpanStartOffset = scanOffset start,
-      sourceSpanEndOffset = scanOffset end
+mkComment :: Maybe Int -> CommentKind -> LexToken -> SourceComment
+mkComment lineHasCode kind tok =
+  SourceComment
+    { commentSpan = lexTokenSpan tok,
+      commentText = lexTokenText tok,
+      commentKind = kind,
+      commentHasCodeBefore = lineHasCode == sourceSpanStartLineMaybe (lexTokenSpan tok)
     }
 
-mkPointSpan :: FilePath -> ScanState -> SourceSpan
-mkPointSpan sourceName st = mkScanSpan sourceName st st
+tokenCountsAsCode :: LexToken -> Bool
+tokenCountsAsCode tok =
+  case (lexTokenOrigin tok, lexTokenKind tok) of
+    (FromSource, TkPragma _) -> False
+    (FromSource, TkEOF) -> False
+    (FromSource, TkError _) -> False
+    (FromSource, _) -> True
+    (InsertedLayout, _) -> False
+
+sourceSpanStartLineMaybe :: SourceSpan -> Maybe Int
+sourceSpanStartLineMaybe sp =
+  case sp of
+    SourceSpan {sourceSpanStartLine = line} -> Just line
+    NoSourceSpan -> Nothing
+
+sourceSpanEndLineMaybe :: SourceSpan -> Maybe Int
+sourceSpanEndLineMaybe sp =
+  case sp of
+    SourceSpan {sourceSpanEndLine = line} -> Just line
+    NoSourceSpan -> Nothing
 
 formatSpan :: SourceSpan -> String
 formatSpan sp =
@@ -233,29 +102,3 @@ formatSpan sp =
     NoSourceSpan -> "<unknown>"
     SourceSpan {sourceSpanSourceName, sourceSpanStartLine, sourceSpanStartCol} ->
       sourceSpanSourceName <> ":" <> show sourceSpanStartLine <> ":" <> show sourceSpanStartCol
-
-byteLength :: Text -> Int
-byteLength = T.length . TE.decodeLatin1 . TE.encodeUtf8
-
-isLineCommentRest :: Text -> Bool
-isLineCommentRest rest =
-  case T.dropWhile (== '-') rest of
-    Empty -> True
-    c :< _ -> not (isSymbolicOpChar c)
-
-isSymbolicOpChar :: Char -> Bool
-isSymbolicOpChar c =
-  c `elem` ("!#$%&*+./<=>?@\\^|-~:" :: String)
-    || (not (isAlphaNum c) && not (isSpace c) && isSymbolicCategory (generalCategory c))
-
-isSymbolicCategory :: GeneralCategory -> Bool
-isSymbolicCategory category =
-  case category of
-    MathSymbol -> True
-    CurrencySymbol -> True
-    ModifierSymbol -> True
-    OtherSymbol -> True
-    DashPunctuation -> True
-    OtherPunctuation -> True
-    ConnectorPunctuation -> True
-    _ -> False
