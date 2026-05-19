728a6c52e9a174762bd06a58784a94028bfdbf0c
David Himmelstrup <lemmih@gmail.com>
2026-05-19 13:43:06 +0200

    refactor(parser): preserve existing view expression parens

--- components/aihc-parser/src/Aihc/Parser/Parens.hs
+++ components/aihc-parser/src/Aihc/Parser/Parens.hs
@@ -1755,18 +1755,30 @@ addViewExprParensWith allowLayoutTypeSig expr =
     addViewExprInnerParens e =
       case e of
         EAnn ann sub -> EAnn ann (addViewExprInnerParens sub)
-        EParen inner ->
-          case peelExprAnn inner of
-            ESectionL {} -> addExprParens inner
-            ESectionR {} -> addExprParens inner
-            EGetFieldProjection {} -> addExprParens inner
-            _ -> EParen (addViewExprInnerParens inner)
+        EParen inner -> EParen (addViewExprInsideExistingParens inner)
         EInfix lhs op rhs ->
           EInfix
             (addViewInfixLhsParens lhs)
             op
             (addExprParensIn (CtxInfixRhs True) rhs)
         _ -> addExprParens e
+
+    addViewExprInsideExistingParens e =
+      case e of
+        EAnn ann sub -> EAnn ann (addViewExprInsideExistingParens sub)
+        ESectionL lhs op -> ESectionL (addSectionLhsInExistingParens lhs) op
+        ESectionR op rhs -> ESectionR op (addExprParensIn CtxSectionRhs rhs)
+        EGetFieldProjection {} -> e
+        _ -> addViewExprInnerParens e
+
+    addSectionLhsInExistingParens lhs =
+      if isGreedyExpr lhs || isTypeSigExpr lhs
+        then wrapExpr True (addExprParens lhs)
+        else addSectionLhsParens lhs
+
+    isTypeSigExpr (EAnn _ sub) = isTypeSigExpr sub
+    isTypeSigExpr ETypeSig {} = True
+    isTypeSigExpr _ = False
 
     addViewInfixLhsParens lhs =
       wrapExpr (viewInfixLhsNeedsParens lhs) (addExprParensIn CtxInfixLhs lhs)
