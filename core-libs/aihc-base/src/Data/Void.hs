9de2e9bbafe1adbbc70b51c22f92745442e72e6b
David Himmelstrup <lemmih@gmail.com>
2026-08-03 08:47:03 +0200

    feat(base): implement Data.Void

--- core-libs/aihc-base/src/Data/Void.hs
+++ core-libs/aihc-base/src/Data/Void.hs
@@ -1,1 +1,18 @@
-module Data.Void () where
+{-# LANGUAGE EmptyDataDecls #-}
+
+module Data.Void
+  ( Void,
+    absurd,
+    vacuous,
+  )
+where
+
+import Prelude (Functor (..), seq)
+
+data Void
+
+absurd :: Void -> a
+absurd impossible = impossible `seq` absurd impossible
+
+vacuous :: (Functor f) => f Void -> f a
+vacuous = fmap absurd
