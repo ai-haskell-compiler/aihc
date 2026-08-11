38243bfb5c4d8a670321cb2468a7652cccbee5f7
David Himmelstrup <lemmih@gmail.com>
2026-08-11 11:11:24 +0200

    fix(grin): keep local nullary constructors local

--- components/aihc-grin/src/Aihc/Grin/Lower.hs
+++ components/aihc-grin/src/Aihc/Grin/Lower.hs
@@ -335,7 +335,11 @@ lowerProgramWithEnvironment linkNames imported local environment program =
           lowerCodeInfosByName = programEnvironmentCodeInfosByName environment,
           lowerLocalCodeLinkNames = localCodeLinkNames,
           lowerReferencedExternalCodeLinkNames = Set.empty,
-          lowerLocalGlobalNames = Set.fromList [linkedName | (_, _, linkedName, _) <- programGlobalInfos linkNames program],
+          lowerLocalGlobalNames =
+            Set.fromList
+              ( [name | (name, arity) <- programConstructors program, arity == 0]
+                  <> [linkedName | (_, _, linkedName, _) <- programGlobalInfos linkNames program]
+              ),
           lowerReferencedExternalGlobalNames = Set.empty,
           lowerGlobalNames = programEnvironmentGlobals environment,
           lowerWhnfGlobalNames = programEnvironmentWhnfGlobals environment,
