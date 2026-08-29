9f1ba0720865975d8ce8959d24a19d49e0ab8e62
David Himmelstrup <lemmih@gmail.com>
2026-08-29 15:18:32 +0200

    feat(base): add Data.Type.Equality :~: and :~~: (#1582)

--- core-libs/aihc-base/src/Data/Type/Equality.hs
+++ core-libs/aihc-base/src/Data/Type/Equality.hs
@@ -1,1 +1,17 @@
-module Data.Type.Equality () where
+{-# LANGUAGE TypeOperators #-}
+
+module Data.Type.Equality
+  ( (:~:) (..),
+    (:~~:) (..),
+  )
+where
+
+infix 4 :~:, :~~:
+
+-- | Propositional equality. A value of type a :~: b proves that a and b are the same type.
+data a :~: b where
+  Refl :: a :~: a
+
+-- | Kind-heterogeneous propositional equality.
+data a :~~: b where
+  HRefl :: a :~~: a
