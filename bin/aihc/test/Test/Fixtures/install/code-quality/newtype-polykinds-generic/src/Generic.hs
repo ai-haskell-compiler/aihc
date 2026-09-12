06c306ea752ba260cf1837f0c86a81df58a9beb7
David Himmelstrup <lemmih@gmail.com>
2026-09-12 12:35:14 +0200

    test: replace mtl install fixtures with source fixtures

--- bin/aihc/test/Test/Fixtures/install/code-quality/newtype-polykinds-generic/src/Generic.hs
+++
@@ -1,6 +0,0 @@
-module Generic where
-
-import GHC.Generics (Generic, Generic1)
-
-newtype Wrapper f a = Wrapper (f a) deriving (Generic, Generic1)
-newtype Constant a b = Constant a deriving (Generic, Generic1)
