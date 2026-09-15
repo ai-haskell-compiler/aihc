e68684e9019ce3e029960f77ab60739b95d4fb01
David Himmelstrup <lemmih@gmail.com>
2026-09-15 12:19:09 +0000

    feat(base): implement GHC.Integer.Logarithms (#2086)

--- core-libs/aihc-base/src/GHC/Num/Integer.hs
+++ core-libs/aihc-base/src/GHC/Num/Integer.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE MagicHash #-}
+
 module GHC.Num.Integer
   ( Integer (..),
+    integerLog2#,
+    integerLogBase#,
   )
 where
 
-import GHC.Internal.Integer (Integer (..))
+import GHC.Internal.Integer (Integer (..), integerLog2#, integerLogBase#)
