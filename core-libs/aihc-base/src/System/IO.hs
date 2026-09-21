89ecf25ff0424cfffe83cf574f90ce10fe0ade14
David Himmelstrup <lemmih@gmail.com>
2026-09-21 08:12:01 +0000

    fix(base): add temporary file functions (#2227)

--- core-libs/aihc-base/src/System/IO.hs
+++ core-libs/aihc-base/src/System/IO.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE CApiFFI #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 -- | The standard IO interface.
 module System.IO
   ( IO,
@@ -62,6 +65,10 @@ module System.IO
     readLn,
     withBinaryFile,
     openBinaryFile,
+    openTempFile,
+    openBinaryTempFile,
+    openTempFileWithDefaultPermissions,
+    openBinaryTempFileWithDefaultPermissions,
     hSetBinaryMode,
     hPutBuf,
     hGetBuf,
@@ -93,6 +100,13 @@ module System.IO
   )
 where
 
+import Data.Bits ((.|.))
+import Data.List (break, elem, reverse)
+import Data.Maybe (Maybe (..))
+import Foreign.C.Error (eEXIST, eINTR, errnoToIOError, getErrno)
+import Foreign.C.String (CString)
+import Foreign.C.Types (CInt (..))
+import GHC.IO (mask_, onException)
 import GHC.IO.Encoding
   ( TextEncoding,
     char8,
@@ -108,10 +122,76 @@ import GHC.IO.Encoding
     utf8_bom,
   )
 import GHC.IO.Handle
+import GHC.IO.Handle.FD (fdToHandle')
 import GHC.IO.Handle.Text (hGetBuf, hGetBufNonBlocking, hGetBufSome, hGetChar, hGetContents, hGetContents', hGetLine, hPutBuf, hPutBufNonBlocking, hPutChar, hPutStr, hPutStrLn, hWaitForInput)
 import GHC.IO.IOMode (IOMode (..))
 import GHC.IO.StdHandles (openBinaryFile, openFile, stderr, stdin, stdout, withBinaryFile, withFile)
-import Prelude (Bool (..), Char, FilePath, IO, Read (..), Show (..), String, appendFile, error, getChar, getContents, getLine, interact, print, pure, putChar, putStr, putStrLn, readFile, readIO, readLn, writeFile, (>>=))
+import GHC.Internal.IO.Types (IOErrorType (..), IOException (..), ioError)
+import System.Posix.Internals (c_unlink, o_CREAT, o_EXCL, o_RDWR, withFilePath)
+import System.Posix.Types (CMode (..))
+import Prelude (Bool (..), Char, FilePath, IO, Integer, Read (..), Show (..), String, appendFile, error, getChar, getContents, getLine, interact, otherwise, print, pure, putChar, putStr, putStrLn, readFile, readIO, readLn, writeFile, (+), (++), (<), (==), (>>=))
+
+-- | Create a private temporary file with a read-write handle.
+openTempFile :: FilePath -> String -> IO (FilePath, Handle)
+openTempFile = openTempFile' "openTempFile" False 0o600
+
+openBinaryTempFile :: FilePath -> String -> IO (FilePath, Handle)
+openBinaryTempFile = openTempFile' "openBinaryTempFile" True 0o600
+
+-- | Apply the process umask to the default file permissions.
+openTempFileWithDefaultPermissions :: FilePath -> String -> IO (FilePath, Handle)
+openTempFileWithDefaultPermissions = openTempFile' "openTempFileWithDefaultPermissions" False 0o666
+
+openBinaryTempFileWithDefaultPermissions :: FilePath -> String -> IO (FilePath, Handle)
+openBinaryTempFileWithDefaultPermissions = openTempFile' "openBinaryTempFileWithDefaultPermissions" True 0o666
+
+openTempFile' :: String -> Bool -> CMode -> FilePath -> String -> IO (FilePath, Handle)
+openTempFile' location binary permissions directory template
+  | '/' `elem` template =
+      ioError (IOError Nothing InvalidArgument location "template contains a path separator" Nothing (Just template))
+  | otherwise = attempt 0
+  where
+    (prefix, suffix) =
+      case break (== '.') (reverse template) of
+        (_, []) -> (template, "")
+        (extension, '.' : rest) -> (reverse rest, '.' : reverse extension)
+        _ -> (template, "")
+    base = case directory of
+      "" -> ""
+      _ -> directory ++ "/"
+    attempt :: Integer -> IO (FilePath, Handle)
+    attempt number = do
+      let path = base ++ prefix ++ show number ++ suffix
+      withFilePath path (create path number)
+    create path number encoded =
+      mask_
+        ( do
+            descriptor <- openTemporaryFile encoded (o_RDWR .|. o_CREAT .|. o_EXCL) permissions
+            if descriptor < 0
+              then do
+                errno <- getErrno
+                if errno == eEXIST
+                  then attempt (number + 1)
+                  else
+                    if errno == eINTR
+                      then attempt number
+                      else ioError (errnoToIOError location errno Nothing (Just path))
+              else do
+                handle <-
+                  fdToHandle' descriptor Nothing False path ReadWriteMode binary
+                    `onException` ( do
+                                      _ <- closeTemporaryFile descriptor
+                                      _ <- c_unlink encoded
+                                      pure ()
+                                  )
+                pure (path, handle)
+        )
+
+foreign import capi unsafe "fcntl.h open"
+  openTemporaryFile :: CString -> CInt -> CMode -> IO CInt
+
+foreign import capi unsafe "unistd.h close"
+  closeTemporaryFile :: CInt -> IO CInt
 
 -- | Handles cannot tell whether they are terminals.
 hIsTerminalDevice :: Handle -> IO Bool
