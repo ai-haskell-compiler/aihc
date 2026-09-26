1972668f11c5a792c5ed971ea3bf9b83d12261e5
David Himmelstrup <lemmih@gmail.com>
2026-09-26 05:07:00 +0000

    fix(core-libs): add the ST and ConstPtr exports that zlib needs (#2305)

--- core-libs/aihc-base/src/Foreign/C/ConstPtr.hs
+++ core-libs/aihc-base/src/Foreign/C/ConstPtr.hs
@@ -1,1 +1,23 @@
-module Foreign.C.ConstPtr () where
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RoleAnnotations #-}
+
+-- | A pointer to a constant C value. A @capi@ wrapper spells a
+-- @ConstPtr a@ as @const T *@, where @T@ is the C spelling of @a@.
+module Foreign.C.ConstPtr
+  ( ConstPtr (..),
+  )
+where
+
+import GHC.Base ((.))
+import GHC.Classes (Eq (..), Ord (..))
+import GHC.Ptr (Ptr)
+import GHC.Show (Show (..), showParen, showString)
+
+type role ConstPtr phantom
+
+newtype {-# CTYPE "const void*" #-} ConstPtr a = ConstPtr {unConstPtr :: Ptr a}
+  deriving newtype (Eq, Ord)
+
+instance Show (ConstPtr a) where
+  showsPrec d (ConstPtr p) = showParen (d > 10) (showString "ConstPtr " . showsPrec 11 p)
